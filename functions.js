efc5f4db0abb8b0bb2b4f7391e644946254fec21
Toufiq <burningraven06@gmail.com>
2018-06-02 15:06:16 +0600

    write basic tests

--- functions.js
+++ functions.js
@@ -1,7 +1,20 @@
-const functions = {
+const allFunctions = {
   add: (num1, num2) => {
     return num1 + num2
+  },
+  isNull : () => {
+    return null;
+  },
+  checkValue: (x) => {
+    return x;
+  },
+  createUser: () => {
+    const user = {
+      fullname: "Don Corleone",
+      email: "test@underground.com"
+    }
+    return user;
   }
 }
 
-module.exports = functions;
+module.exports = allFunctions;
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,55 @@
-const functions = require('./functions');
+const allFunctions = require('./functions');
 
 test('Adds 2 + 2 to equal 4', () => {
-  expect(functions.add(2,2)).toBe(4);
+  expect(allFunctions.add(2,2)).toBe(4);
 });
+
+test('Adds 3 + 3 to not equal 5', () => {
+  expect(allFunctions.add(3,3)).not.toBe(5);
+});
+
+test('Should be null', () => {
+  expect(allFunctions.isNull()).toBeNull();
+});
+
+test('Should be Falsy', () => {
+  expect(allFunctions.checkValue(null)).toBeFalsy();
+  expect(allFunctions.checkValue(0)).toBeFalsy();
+  expect(allFunctions.checkValue(undefined)).toBeFalsy();
+});
+
+test('User should be DC', () => {
+  const userObj = {
+    fullname: 'Don Corleone',
+    email : 'test@underground.com'
+  }
+  expect( allFunctions.createUser() ).toEqual(userObj);
+
+  const contactObj = {
+    fullname: 'Don Corleone',
+    cell : '+00142124745'
+  }
+  expect( allFunctions.createUser() ).toEqual(contactObj);
+});
+
+
+test("Should be under 1400", () => {
+  const load1 = 800;
+  const load2 = 600;
+  const totalLoad = load1 + load2;
+  expect(totalLoad).toBeLessThan(1400);
+});
+
+test("There is no I in team", () => {
+  expect('team').not.toMatch(/I/);
+});
+
+test('Admin should be in usernames', () => {
+  const usernames = ['mark', 'ben', 'harry'];
+  expect(usernames).toContain('admin');
+});
+
+test('Fish should be in items', () => {
+  const items = ['fish', 'cat', 'fly'];
+  expect(items).toContain('fish');
+})
